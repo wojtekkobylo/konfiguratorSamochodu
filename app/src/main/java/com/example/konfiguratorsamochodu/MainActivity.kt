99f8b92019f4619da44f183a8a6ef15f3d0ee1a0
Maks <dawidjasperwojcik@gmail.com>
2025-03-17 10:43:38 +0100

    imageView change + layout

--- app/src/main/java/com/example/konfiguratorsamochodu/MainActivity.kt
+++ app/src/main/java/com/example/konfiguratorsamochodu/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.konfiguratorsamochodu
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +21,33 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        val checkBoxKlima = findViewById<CheckBox>(R.id.checkKlima)
+        val checkBoxSiedzenia = findViewById<CheckBox>(R.id.checkBoxSiedzenia)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val buttonKonfig = findViewById<Button>(R.id.buttonKonfig)
+
+        var rodzajAuta =""
+        var autoZdjecie = R.drawable.suv
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            autoZdjecie = when (checkedId) {
+                R.id.radioSUV -> R.drawable.suv
+                R.id.radioSedan -> R.drawable.sedan
+                R.id.radioHatchBack -> R.drawable.hatchback
+                else -> R.drawable.suv
+            }
+
+            rodzajAuta = when (checkedId) {
+                R.id.radioSUV -> "SUV"
+                R.id.radioSedan -> "Sedan"
+                R.id.radioHatchBack -> "Hatchback"
+                else -> ""
+            }
+
+
+            imageView.setImageResource(autoZdjecie)
         }
     }
 }
